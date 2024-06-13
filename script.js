a3ae76cfc355ab0d68c913ab3d87ebb480f258d7
Sujan Khapung <sujanlimbu8@gmail.com>
2024-06-13 21:51:25 +1000

    new files

---
+++ script.js
@@ -0,0 +1,22 @@
+// function Book(title, author, pages, haveRead){
+//     this.title = title;
+//     this.author = author;
+//     this.pages = pages;
+//     this.haveRead = haveRead;
+//     this.info = function(){
+//         if(this.haveRead)
+//             return title+' by '+author+', '+pages+' pages, already read.'
+//         else
+//         return title+' by '+author+', '+pages+' pages, not read yet.'
+//     }
+// }
+
+const myLibrary = [];
+
+function Book(){
+
+}
+
+function addBookToLibrary(){
+    
+}
